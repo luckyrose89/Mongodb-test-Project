4fd5755d061cec4499591c51a3835f3be5ee0013
avidrutham <divyamathur89@gmail.com>
2018-09-17 15:00:16 +0530

    add post route to app

--- server/server.js
+++ server/server.js
@@ -1,60 +1,26 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var {mongoose} = require('./db/db.js');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/users');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+       res.send(doc); 
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
-// var newTodo = new Todo({
-//     text: 'Code 2 hours'
-// });
-
-// newTodo.save().then((doc) => {
-//     console.log(doc);
-// }, (e) => {
-//     console.log('Unable to save entry to db');
-// });
-
-// var myTodo = new Todo({
-//     text: 'complete this project '
-// });
-
-// myTodo.save().then((doc) => {
-//     console.log(JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//     console.log('Unable to save entry to db');
-// });
-
-var User = mongoose.model('user', {
-    email: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    }
-});
-
-var divya = new User({
-    email: 'divyamathur@abc.com'
-});
-
-divya.save().then((doc) =>{
-    console.log(JSON.stringify(doc, undefined, 2));
-}, (err) => {
-    console.log('Could not add to database!!');
+app.listen(3000, () => {
+    console.log("Started on port 3000");
 });
